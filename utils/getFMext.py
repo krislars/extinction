e1b15569d81f7da64c3cec13dfc502422b643c3d
krislars <krislars@gmail.com>
2017-09-06 18:01:59 -0700

    creating utils directory

--- utils/getFMext.py
+++ utils/getFMext.py
@@ -3,15 +3,14 @@ def getFMext(wave,R,source):
     """ 
         Input: wavelength in microns, nominal R = 3.1
             source='f99' for Fitzpatrick 1999 or ='fmunred' for fmunred.pro
-
-        Output: Al/EBV, so user must divide by R to get Al/AV
+            source='f99 tables' reproduces common misunderstanding from that paper
+        Output: Al/EBV, so user must divide by R to get Al/AV !!!
     """
     
-    from scipy.interpolate import interp1d
-    
     import numpy as np
-
-    x_anchors = 1.0E4 / np.array([np.inf, 26500., 12200., 6000., 5470., 4670., 4110.]) #microns
+    import scipy.interpolate as interp
+    
+    x_anchors = 1.0E4 / np.array([np.inf, 26500., 12200., 6000., 5470., 4670., 4110.]) # microns
     
     if source=="f99 tables": # Don't use, for demonstration only
         a26500= 0.265
@@ -20,7 +19,8 @@ def getFMext(wave,R,source):
         a5470 = -0.050 +1.0016*R
         a4670 =  0.701 +1.0016*R
         a4110 =  1.208 +1.0032*R -0.00033*R**2.0 # typo in the paper -KAL
-    if source=="f99": # Don't use, for demonstration only
+    
+    elif source=="f99": 
         a26500= 0.265*R/3.1
         a12200= 0.829*R/3.1
         a6000 = -0.426 +1.0044*R
@@ -29,7 +29,7 @@ def getFMext(wave,R,source):
         a4110 =  1.208 +1.0032*R -0.00033*R**2.0 # typo in the paper -KAL
 
     elif source=="fmunred":
-        a26500= 0.26469*R/3.1    # R-dependent IR is the big change here -KAL
+        a26500= 0.26469*R/3.1
         a12200= 0.82925*R/3.1
         a6000 = -4.22809e-01 +1.00270*R+2.13572e-04*R**2.0
         a5470 = -5.13540e-02 +1.00216*R-7.35778e-05*R**2.0
@@ -38,7 +38,7 @@ def getFMext(wave,R,source):
 
     a_anchors = np.array([0.0, a26500, a12200, a6000, a5470, a4670, a4110])
 
-    f=interp1d(x_anchors, a_anchors, kind='cubic')    
+    f=interp.interp1d(x_anchors, a_anchors, kind='cubic')    
     
     
     return f(1.0/wave)
--- utils/getMag.py
+++ utils/getMag.py
@@ -11,25 +11,25 @@ def getMag(band,wavelength,flux):
     # NOTE: ugriz filters are on the AB magnitude system, UBVRIJHKs are on the Vega system
     
     if band=='Ks':
-        bandwav,bandpass=np.loadtxt('../DATA/filters/Ks_2MASS.txt',unpack=True)
+        bandwav,bandpass=np.loadtxt('utils/filters/Ks_2MASS.txt',unpack=True)
         center=2.159        # micron
         F0=4.283E-10        # W m^-2 micron^-1
         dlambda=0.262
 
     elif band=='H':
-        bandwav,bandpass=np.loadtxt('../DATA/filters/H_2MASS.txt',unpack=True)
+        bandwav,bandpass=np.loadtxt('utils/filters/H_2MASS.txt',unpack=True)
         center=1.662
         F0=1.133E-9
         dlambda=0.251
 
     elif band=='J':
-        bandwav,bandpass=np.loadtxt('../DATA/filters/J_2MASS.txt',unpack=True)
+        bandwav,bandpass=np.loadtxt('utils/filters/J_2MASS.txt',unpack=True)
         center=1.235
         F0=3.129E-9
         dlambda=0.162
 
     elif band=='U':
-        bandwav_A,bandpass_100=np.loadtxt('../DATA/filters/Bessel_U.txt',unpack=True)
+        bandwav_A,bandpass_100=np.loadtxt('utils/filters/Bessel_U.txt',unpack=True)
         bandwav=bandwav_A/1.0E3
         bandpass=bandpass_100/100.0
         center=0.365
@@ -37,7 +37,7 @@ def getMag(band,wavelength,flux):
         dlambda=0.066
 
     elif band=='B':
-        bandwav_A,bandpass_100=np.loadtxt('../DATA/filters/Bessel_B.txt',unpack=True)
+        bandwav_A,bandpass_100=np.loadtxt('utils/filters/Bessel_B.txt',unpack=True)
         bandwav=bandwav_A/1.0E3
         bandpass=bandpass_100/100.0
         center=0.445
@@ -45,7 +45,7 @@ def getMag(band,wavelength,flux):
         dlambda=0.094
 
     elif band=='V':
-        bandwav_A,bandpass_100=np.loadtxt('../DATA/filters/Bessel_V.txt',unpack=True)
+        bandwav_A,bandpass_100=np.loadtxt('utils/filters/Bessel_V.txt',unpack=True)
         bandwav=bandwav_A/1.0E3
         bandpass=bandpass_100/100.0
         center=0.551
@@ -53,7 +53,7 @@ def getMag(band,wavelength,flux):
         dlambda=0.088
 
     elif band=='R':
-        bandwav_A,bandpass_100=np.loadtxt('../DATA/filters/Bessel_R.txt',unpack=True)
+        bandwav_A,bandpass_100=np.loadtxt('utils/filters/Bessel_R.txt',unpack=True)
         bandwav=bandwav_A/1.0E3
         bandpass=bandpass_100/100.0
         center=0.658
@@ -61,7 +61,7 @@ def getMag(band,wavelength,flux):
         dlambda=0.138
 
     elif band=='I':
-        bandwav_A,bandpass_100=np.loadtxt('../DATA/filters/Bessel_I.txt',unpack=True)
+        bandwav_A,bandpass_100=np.loadtxt('utils/filters/Bessel_I.txt',unpack=True)
         bandwav=bandwav_A/1.0E3
         bandpass=bandpass_100/100.0
         center=0.806
@@ -84,17 +84,8 @@ def getMag(band,wavelength,flux):
     filterband[inband]=bandinterp(wavelength[inband])
             # Now filterband is the same shape as wavelength and flux, zeros and bandpass
     
-    dwav=np.zeros(wavelength.size)
-        
-    dwav[0:-1]=np.abs(wavelength[1:]-wavelength[0:-1]) # base of the rectangles for simple integration
     
     mag=2.5*np.log10(F0*dlambda/integrate.trapz(flux*filterband, x=wavelength))
-
-
-    return mag
-
-    
-    mag=2.5*np.log10(F0*dlambda/(np.sum(flux*dwav*filterband)))
 
 
     return mag
